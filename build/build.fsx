ac7bdc0c5c01e3c7cc887c266f82e7572ddc0bb2
Adrian Macal <adrian.macal@check24.de>
2016-06-21 14:26:15 +0200

    Update package dependencies.

--- build/build.fsx
+++ build/build.fsx
@@ -41,7 +41,7 @@ Target "ExecuteTests" (fun _ ->
 Target "CreatePackage" (fun _ ->
      NuGet (fun p -> 
         { p with
-            Version = "1.1"
+            Version = "1.2"
             OutputPath = "./build/package"
             WorkingDir = "./build/release"
             Dependencies = []
